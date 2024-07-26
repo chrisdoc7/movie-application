e0fe82b126b9cc23ccf9e89d880cc675d8d9b70f
Christian Ospina <chrisdoc7@hotmail.com>
2024-07-25 19:31:01 -0500

    Test GIT Gui

--- 1_total-budget-by-actors.sql
+++ 1_total-budget-by-actors.sql
@@ -4,7 +4,7 @@ SELECT p.id_person, p.firstname, p.lastname, SUM(m.budget) +
 		FROM person p
 		INNER JOIN movie_actor ma ON p.id_person = ma.id_person
 		INNER JOIN movie m ON ma.id_movie = m.id_movie
-	) as total_budget
+	) AS total_budget
 FROM person p
 INNER JOIN personage pg ON p.id_person = pg.id_person
 INNER JOIN movie_personage mp ON pg.id_personage = mp.id_personage
